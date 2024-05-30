11349c87deb7077939ca9aa8f52469f07b98da96
Ricky745 <tintoriccardo745@gmail.com>
2024-05-30 12:19:26 +0000

    ciao soliman

--- sito/src/app/app.component.ts
+++ sito/src/app/app.component.ts
@@ -2,11 +2,13 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { AlbumComponent } from './album/album.component';
 import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { RegistrazioneComponent } from './registrazione/registrazione.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,AlbumComponent,HomeComponent],
+  imports: [RouterOutlet,AlbumComponent,HomeComponent,LoginComponent,RegistrazioneComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
